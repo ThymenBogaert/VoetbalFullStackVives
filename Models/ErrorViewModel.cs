40b86355dbea8d9ab5cf3c254c22bf941106cfa5
ThymenBogaert <r0808902@student.vives.be>
2022-04-06 14:33:31 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VoetbalFullStack.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
